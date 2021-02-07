90bf7138d15ddb6e021729222c3aac594f26c570
steven <stevenragy67>
2021-02-07 15:07:58 +0200

    Add spouse to add child

--- src/functions.tsx
+++ src/functions.tsx
@@ -1,28 +1,27 @@
 import { Node } from "./types";
 
-export function AddNode(
-  parent: Node | null | undefined,
-  child: Node | null | undefined
-) {
-  if (parent && child) {
-    if (parent.children) {
-      parent.children.push(child);
-    } else {
-      parent.children = [child];
+export function AddNode(parent: Node | null | undefined, child: Node | null | undefined, type: string) {
+    if (parent && child) {
+        if (type === "child") {
+            if (parent.children) {
+                parent.children.push(child);
+            } else {
+                parent.children = [child];
+            }
+            child.parent = parent;
+        } else {
+            parent.spouse = child;
+        }
     }
-    child.parent = parent;
-  }
 }
 
-export function DeleteNode(
-  parent: Node | null | undefined,
-  childToRemoveName: string | undefined
-) {
-  if (parent !== null && parent !== undefined) {
-    parent.children?.map((child, index) => {
-      if (child.name === childToRemoveName) {
-        parent.children?.splice(index, 1);
-      }
-    });
-  }
+export function DeleteNode(parent: Node | null | undefined, childToRemoveName: string | undefined) {
+    if (parent !== null && parent !== undefined) {
+        // eslint-disable-next-line
+        parent.children?.map((child, index) => {
+            if (child.name === childToRemoveName) {
+                parent.children?.splice(index, 1);
+            }
+        });
+    }
 }
