40137a77c108bbd1897b7a153100b3d79a99c61f
Ahmed Habeila <sm3rta@gmail.com>
2021-02-07 16:03:55 +0200

    initialize node dialog+minor bug fixes

--- src/App.tsx
+++ src/App.tsx
@@ -5,41 +5,44 @@ import grandparent from "./mockData";
 import { Node } from "./types";
 import AddDeleteNodeDialog from "./AddDeleteNodeDialog";
 import CustomNodeElement from "./CustomNodeElement";
-import { inheritanceCalculation } from "./inhertitance-calculations"; 
+import { inheritanceCalculation } from "./inhertitance-calculations";
 // import grandparent from './mockData'
+
 function App() {
-  const [selectedNode, setSelectedNode] = useState<Node | null>(null);
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
-  const showDialog = () => setIsDialogOpen(true);
-  const hideDialog = () => setIsDialogOpen(false);
-  console.log(inheritanceCalculation(grandparent))
-  return (
-    <>
-      <AddDeleteNodeDialog
-        isDialogOpen={isDialogOpen}
-        hideDialog={hideDialog}
-        selectedNode={selectedNode}
-      />
-      <Tree
-        data={grandparent}
-        orientation="vertical"
-        translate={{
-          x: document.body.clientWidth / 2,
-          y: document.body.clientHeight / 5,
-        }}
-        renderCustomNodeElement={(props) => (
-          <CustomNodeElement
-            {...props}
-            setSelectedNode={setSelectedNode}
-            showDialog={showDialog}
-          />
-        )}
-        pathFunc="step"
-        collapsible={false}
-        depthFactor={250}
-      />
-    </>
-  );
+	const [selectedNode, setSelectedNode] = useState<Node | null>(null);
+	const [isDialogOpen, setIsDialogOpen] = useState(false);
+	const showDialog = () => setIsDialogOpen(true);
+	const hideDialog = () => setIsDialogOpen(false);
+	console.log(inheritanceCalculation(grandparent));
+	const [root, setRoot] = useState<Node | null>(null);
+	return (
+		<>
+			<AddDeleteNodeDialog
+				isDialogOpen={isDialogOpen || !root}
+				hideDialog={hideDialog}
+				selectedNode={selectedNode}
+				setRoot={setRoot}
+				setSelectedNode={setSelectedNode}
+			/>
+			{root && (
+				<Tree
+					data={root}
+					// data={grandparent}
+					orientation="vertical"
+					translate={{
+						x: document.body.clientWidth / 2,
+						y: document.body.clientHeight / 5,
+					}}
+					renderCustomNodeElement={(props) => (
+						<CustomNodeElement {...props} setSelectedNode={setSelectedNode} showDialog={showDialog} />
+					)}
+					pathFunc="step"
+					collapsible={false}
+					depthFactor={250}
+				/>
+			)}
+		</>
+	);
 }
 
 export default App;
--- src/useStyles.ts
+++ src/useStyles.ts
@@ -1,28 +1,28 @@
 import { makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-  livingNode: {
-    fill: "lavender",
-  },
-  deadNode: {
-    fill: "black",
-  },
-  button: {
-    minWidth: 250,
-    maxWidth: 250,
-  },
-  selectedButton: {
-    color: "blue",
-  },
-  centeredFlex: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  columnFlex: { flexDirection: "column" },
-  spaceBetween: {
-    justifyContent: "space-between",
-  },
+	livingNode: {
+		fill: "lavender",
+	},
+	deadNode: {
+		fill: "black",
+	},
+	button: {
+		minWidth: 250,
+		maxWidth: 250,
+	},
+	selectedButton: {
+		color: "blue",
+	},
+	centeredFlex: {
+		display: "flex",
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	columnFlex: { flexDirection: "column" },
+	spaceBetween: {
+		justifyContent: "space-between",
+	},
 }));
 
 export default useStyles;
