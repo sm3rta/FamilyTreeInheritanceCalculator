1ea692eb15d52e96f9ebe3ae121445615e676e6c
steven <stevenragy67>
2021-02-07 13:55:20 +0200

    Delete function

--- src/functions.tsx
+++ src/functions.tsx
@@ -3,9 +3,10 @@ import { Node } from "./types";
 
 export function AddNode(parent: Node, child: Node) {
     parent.children?.push(child);
+    child.parent = parent;
 }
 
-export function DeletNode(parent: Node, childToRemoveName: string) {
+export function DeleteNode(parent: Node, childToRemoveName: string) {
     parent.children?.map((child, index) => {
         if (child.name === childToRemoveName) {
             parent.children?.splice(index, 1);
